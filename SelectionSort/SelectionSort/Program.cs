e7e05c9d1ddc6788cf37227b1e0f892a0b092123
Yonas Keflemariam <yonas@sphynxsoftware.com>
2017-03-19 17:14:56 -0400

    commit latest changes...

--- SelectionSort/SelectionSort/Program.cs
+++ SelectionSort/SelectionSort/Program.cs
@@ -8,17 +8,26 @@ namespace Algorithmns_SelectionSort
 {
     class Program
     {
+        //Console program entry point
         static void Main(string[] args)
         {
+            //Sample data values
             int array_size = 10;
             int[] array = new int[10] { 100, 50, 20, 40, 10, 60, 80, 70, 90, 30 };
+
+            
             Console.WriteLine("The Array Before Selection Sort is: ");
+
+            //Initial array output (to console)
             for (int i = 0; i < array_size; i++)
             {
                 Console.WriteLine(array[i]);
             }
+
+            //new variable initialization
             int tmp, min_key;
 
+            //outer and inner loops (w/comparison)
             for (int j = 0; j < array_size - 1; j++)
             {
                 min_key = j;
@@ -36,6 +45,7 @@ namespace Algorithmns_SelectionSort
                 array[j] = tmp;
             }
 
+            //post sort output (to console)
             Console.WriteLine("The Array After Selection Sort is: ");
             for (int i = 0; i < 10; i++)
             {

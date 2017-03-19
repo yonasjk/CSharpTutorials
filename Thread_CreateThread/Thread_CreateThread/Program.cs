5511343fe5a94a0ea6d2bf4be3f5eea6b4bb44f2
Yonas Keflemariam <yonas@sphynxsoftware.com>
2017-03-19 09:19:33 -0400

    Additional Commit

---
+++ Thread_CreateThread/Thread_CreateThread/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections;
+using System.Threading;
+
+namespace Mutexclass
+{
+    class Thread_MutexProtectedResource
+    {
+        private static Mutex mutex = new Mutex();
+        private const int numhits = 1;
+        private const int numThreads = 4;
+
+        private static void ThreadProcess()
+        {
+            for (int i = 0; i < numhits; i++)
+            {
+                UseCsharpcorner();
+            }
+        }
+
+        private static void UseCsharpcorner()
+        {
+            mutex.WaitOne();   // Wait until it is safe to enter.
+            Console.WriteLine("{0} has entered in the C_sharpcorner.com",
+                Thread.CurrentThread.Name);
+            // Place code to access non-reentrant resources here.
+            Thread.Sleep(500);    // Wait until it is safe to enter.
+            Console.WriteLine("{0} is leaving the C_sharpcorner.com\r\n",
+                Thread.CurrentThread.Name);
+            mutex.ReleaseMutex();    // Release the Mutex.
+        }
+
+        static void Main(string[] args)
+        {
+            for (int i = 0; i < numThreads; i++)
+            {
+                Thread mycorner = new Thread(new ThreadStart(ThreadProcess));
+                mycorner.Name = String.Format("Thread{0}", i + 1);
+                mycorner.Start();
+            }
+            Console.Read();
+        }
+
+    }
+}

b1a6657c46f4161cd5ad3b7743f731ec9208e1c8
Yonas Keflemariam <yonas@sphynxsoftware.com>
2017-03-18 22:08:32 -0400

    Commit (Several)

--- BinarySearch/BinarySearch/Program.cs
+++ BinarySearch/BinarySearch/Program.cs
@@ -6,10 +6,40 @@ using System.Threading.Tasks;
 
 namespace BinarySearch
 {
-    class Program
+    class BinarySearchTest
     {
         static void Main(string[] args)
         {
+            int searchInt; // seach key
+            int position; // location of search key in array
+
+            // create array and output it
+            BinaryArray searchArray = new BinaryArray(15);
+            Console.WriteLine(searchArray);
+
+            // prompt and input first int from user
+            Console.Write("Please enter an integer value (-1 to quit): ");
+            searchInt = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine();
+
+            // repeatedly input an integer; -1 terminates the application
+            while (searchInt != -1)
+            {
+                // use binary search to try to find integer
+                position = searchArray.BinarySearch(searchInt);
+
+                // return value of -1 indicates integer was not found
+                if (position == -1)
+                    Console.WriteLine("The integer {0} was not found.\n", searchInt);
+                else
+                    Console.WriteLine("The integer {0} was found in position {1}.\n", searchInt, position);
+
+                // Prompt and input next int from user 
+                Console.WriteLine("Please enter an integer value (-1 to quit): ");
+                searchInt = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine();
+
+            }
         }
     }
 }
